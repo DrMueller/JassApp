d8b438dea7cc5a40497c3a63abb76975aa3a4245
Müller Matthias <matthias.mueller@noser.com>
2025-08-26 18:55:32 +0200

    WIP

--- Sources/Application/Domain/Jassrunden/Models/JasskarteWert.cs
+++ Sources/Application/Domain/Jassrunden/Models/JasskarteWert.cs
@@ -1,4 +1,7 @@
-﻿namespace JassApp.Domain.Jassrunden.Models
+﻿using JetBrains.Annotations;
+
+namespace JassApp.Domain.Jassrunden.Models
 {
+    [PublicAPI]
     public record JasskarteWert(JasskarteWertTyp JasskarteWertTyp, string Label);
 }
