da2cf9e878de1ef6991599eb0c8022b3037fdeca
Müller Matthias <matthias.mueller@noser.com>
2025-09-15 16:07:08 +0200

    WIP

---
+++ Sources/Application/Presentation/Areas/Test/Components/TestInformationsPage.razor.cs
@@ -0,0 +1,21 @@
+﻿using JassApp.Common.InformationHandling;
+
+namespace JassApp.Presentation.Areas.Test.Components
+{
+    public partial class TestInformationsPage
+    {
+        private const string Path = "/test/informations";
+        private InformationEntries? Infos { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Infos = InformationEntries
+                .CreateFromError("This is an error")
+                .AddError("This is an error2")
+                .AddWarning("This is a warning")
+                .AddWarning("This is a warning2")
+                .AddInformation("This is an info")
+                .AddInformation("This is an info1");
+        }
+    }
+}
