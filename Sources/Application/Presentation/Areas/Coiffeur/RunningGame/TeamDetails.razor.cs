fd0f0502c427af64fbdfe6d68eb77822432d5695
Müller Matthias <matthias.mueller@noser.com>
2026-02-10 16:32:49 +0100

    WIP

--- Sources/Application/Presentation/Areas/Coiffeur/RunningGame/TeamDetails.razor.cs
+++ Sources/Application/Presentation/Areas/Coiffeur/RunningGame/TeamDetails.razor.cs
@@ -1,11 +1,14 @@
 ﻿using JassApp.Domain.Coiffeur.Models;
-using JassApp.Presentation.Shared.Voices;
 using Microsoft.AspNetCore.Components;
+using Toolbelt.Blazor.SpeechSynthesis;
 
 namespace JassApp.Presentation.Areas.Coiffeur.RunningGame
 {
     public partial class TeamDetails
     {
+        [Inject]
+        public required SpeechSynthesis SpeechSynthesis { get; set; }
+
         [Parameter]
         [EditorRequired]
         public required CoiffeurSpielrunde Spielrunde { get; set; }
@@ -16,13 +19,14 @@ namespace JassApp.Presentation.Areas.Coiffeur.RunningGame
 
         private string TeamDescription => Spielrunde.GetTeamDescription(TeamTyp);
 
-        private Voice VoiceRef { get; set; } = null!;
+        //private Voice VoiceRef { get; set; } = null!;
 
         public async Task HandleOpenTruempfeRequestedAsync()
         {
             var offeneTruempfe = Spielrunde.GetOffeneTruempfeDescription(TeamTyp);
 
-            await VoiceRef.SpeakAsync(offeneTruempfe);
+            await SpeechSynthesis.SpeakAsync(offeneTruempfe);
+            //await VoiceRef.SpeakAsync(offeneTruempfe);
         }
     }
 }
